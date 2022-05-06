446786a9feff0ef075f225cfb657e73cfff12d3a
chris <chrisotpherkelly92@gmail.com>
2022-05-05 22:36:34 -0700

    refactored; put in more params; added comments

--- Simulation/model.py
+++ Simulation/model.py
@@ -1,4 +1,5 @@
 import json
+# had issues with params file location between windows and macOS
 from pathlib import Path
 script_location = Path(__file__).absolute().parent
 params_location = script_location / 'params.json'
@@ -7,11 +8,31 @@ params_location = script_location / 'params.json'
 class Model:
     def __init__(self):
         with open(params_location) as json_file:
-            self.param_vals = json.load(json_file)
+            self.params = json.load(json_file)
 
-    def save_params(self,param_vals):
+    def save_params(self,params:dict):
         with open(params_location, 'w') as outfile:
-            json.dump(param_vals, outfile,indent=4)
+            json.dump(params, outfile,indent=4)
             
-    def sum_yearly_budget(self,monthly_list,yearly_list):
-        return sum([12*n for n in monthly_list])+sum(yearly_list)
+    def run_calcs(self,params:dict):
+        params = self._clean_data(params)
+        params["Domestic Proportion"]=1-params["International Proportion"]
+        return params
+
+    def _clean_data(self,params:dict):
+        for k,v in params.items():
+            if v.isdigit():
+                params[k]=int(v)
+            elif self._is_float(v):
+                params[k]=float(v)
+            elif v == "True" or v == "False":
+                params[k]=bool(v)
+        return params
+
+    def _is_float(self,element:any):
+        try:
+            float(element)
+            return True
+        except ValueError:
+            return False
+        
--- Simulation/view.py
+++ Simulation/view.py
@@ -3,29 +3,37 @@ from tkinter import ttk
 from dualWidgetFrame import DWFrame
 
 class View(tk.Tk):
-    def __init__(self,controller):
+    def __init__(self,controller,CALCD_PARAMS:list):
         super().__init__()
         self.title("Life Finances")
-        self.minsize(width=500, height=500)
+        #self.minsize(width=500, height=500)
         
-        self.controller = controller
+        self.controller = controller #using in DWFrame class to acess controller.update command
         
+        self.CALCD_PARAMS = CALCD_PARAMS #used to disable entries and prevent overwriting non-calcd entries
         self.param_frames = {}
                 
     def main(self):
         self.mainloop()
         
-    def make_frames(self,param_vals,CALCD_PARAMS):
-        for k,v in param_vals.items():
-            frame = DWFrame(window=self,key=k,value=v,enabled=(False if k in CALCD_PARAMS else True))
+    def make_frames(self,params:dict):
+        """Make a frame for each parameter. Frame includes a label and appropriate second widget"""
+        for k,v in params.items():
+            frame = DWFrame(window=self,key=k,value=v,enabled=(False if k in self.CALCD_PARAMS else True))
             frame.pack()
             self.param_frames[k] = frame
     
-    def get_param_vals(self):
-        param_vals = {}
+    def get_params(self):
+        """Pull values from view and return params dict"""
+        params = {}
         for k in self.param_frames.keys():
-            param_vals[k]=float(self.param_frames[k].val_var.get())
-        return param_vals
-    
-    def set_val(self,key,value):
-        self.param_frames[key].val_var.set(value)
+            params[k]=self.param_frames[k].val_var.get()
+        return params
+        
+    def set_params(self, params: dict):
+        """Update all calculated entries to passed-in params values"""
+        for key,value in params.items():
+            if key not in self.CALCD_PARAMS:
+                continue
+            self.param_frames[key].val_var.set(value)
+        
