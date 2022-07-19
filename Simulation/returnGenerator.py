be4b2a3648cba5996dd40001e4a6c12f6981fa73
kellychris_work <christopherkelly92@gmail.com>
2022-07-19 16:21:15 -0700

    Issues 11, 12, 16

--- Simulation/returnGenerator.py
+++ Simulation/returnGenerator.py
@@ -4,29 +4,9 @@ from scipy import stats
 import random
 import numpy as np
 from skewDist import createSkewDist
+import constants as const
 
 DEBUG_LVL = 0
-
-EQUITY_MEAN = 1.095
-EQUITY_STDEV = .16
-EQUITY_ANNUAL_HIGH = 1.09 # limits for annualized return check
-EQUITY_ANNUAL_LOW = 1.07
-
-BOND_MEAN = 1.02
-BOND_STDEV = .025
-BOND_ANNUAL_HIGH = 1.02
-BOND_ANNUAL_LOW = 1.015
-
-RE_MEAN = 1.11
-RE_STDEV = .14
-RE_ANNUAL_HIGH = 1.12
-RE_ANNUAL_LOW = 1.08
-
-INFLATION_MEAN = 1.037  # https://fred.stlouisfed.org/series/FPCPITOTLZGUSA#
-INFLATION_STDEV = .027
-INFLATION_ANNUAL_HIGH = 1.063
-INFLATION_ANNUAL_LOW = 1.020
-INFLATION_SKEW = 1.642
 
 years_qty = 90
 
@@ -107,21 +87,21 @@ def generate_skewd_inflation(mean, stdev, skew,qty_per_column,qty_per_year,colum
 
 def main(qty_per_column,qty_per_year,columns):
     generated_array =[]
-    generated_array.append(generate_returns(EQUITY_MEAN, EQUITY_STDEV, EQUITY_ANNUAL_HIGH, EQUITY_ANNUAL_LOW,
+    generated_array.append(generate_returns(const.EQUITY_MEAN, const.EQUITY_STDEV, const.EQUITY_ANNUAL_HIGH, const.EQUITY_ANNUAL_LOW,
                                     qty_per_column,qty_per_year,columns, file_name="StockReturns.csv"))
-    generated_array.append(generate_returns(BOND_MEAN, BOND_STDEV, BOND_ANNUAL_HIGH, BOND_ANNUAL_LOW,
+    generated_array.append(generate_returns(const.BOND_MEAN, const.BOND_STDEV, const.BOND_ANNUAL_HIGH, const.BOND_ANNUAL_LOW,
                                     qty_per_column,qty_per_year,columns, file_name="BondReturns.csv"))
     if DEBUG_LVL >= 1: start = time.perf_counter()
-    generated_array.append(generate_returns(RE_MEAN, RE_STDEV, RE_ANNUAL_HIGH, RE_ANNUAL_LOW,
+    generated_array.append(generate_returns(const.RE_MEAN, const.RE_STDEV, const.RE_ANNUAL_HIGH, const.RE_ANNUAL_LOW,
                                     qty_per_column,qty_per_year,columns, file_name="REReturns.csv"))
     if DEBUG_LVL >= 1: 
         mid = time.perf_counter()
-        speed_test = generate_returns_faster(RE_MEAN, RE_STDEV, RE_ANNUAL_HIGH, RE_ANNUAL_LOW,
+        speed_test = generate_returns_faster(const.RE_MEAN, const.RE_STDEV, const.RE_ANNUAL_HIGH, const.RE_ANNUAL_LOW,
                                         qty_per_column,qty_per_year,columns, file_name="REReturns.csv")
         end = time.perf_counter()
     # generated_array.append(generate_inflation(INFLATION_MEAN, INFLATION_STDEV, INFLATION_ANNUAL_HIGH, INFLATION_ANNUAL_LOW,
     #                                 qty_per_column,qty_per_year,columns, file_name="Inflation.csv"))
-    generated_array.append(generate_skewd_inflation(INFLATION_MEAN, INFLATION_STDEV, INFLATION_SKEW,
+    generated_array.append(generate_skewd_inflation(const.INFLATION_MEAN, const.INFLATION_STDEV, const.INFLATION_SKEW,
                                     qty_per_column,qty_per_year,columns))
     if DEBUG_LVL >= 1: 
         print(f"standard: {mid-start}")
