886e384c9d52359479517231ea89a027506d4c9b
kellychris_work <christopherkelly92@gmail.com>
2022-08-23 18:44:18 -0700

    first stab at issue #31

--- models/annuity.py
+++ models/annuity.py
@@ -1,11 +1,12 @@
 
 class Annuity:
-    def __init__(self,interest_rate_qt,payout_rate_qt,time_ls:list):
-        self.interest_rate = interest_rate_qt
+    def __init__(self,interest_yield_qt,payout_rate_qt,time_ls:list):
+        self.interest_yield = interest_yield_qt
         self.payout_rate = payout_rate_qt
         self.time_ls = time_ls
         self.transactions = [0 for _ in time_ls] #net contibutions/withdrawals
-        self.balances = []
+        self.balances = [0 for _ in time_ls]
+        self.annuitized = False
     
     def contribute(self,amount,date):
         self.transactions[self.time_ls.index(date)] += amount
@@ -16,18 +17,26 @@ class Annuity:
         for i, amount in enumerate(self.transactions):
             self.balances.append(amount)
             if i != 0: 
-                self.balances[i] += self.balances[i-1] * self.interest_rate
+                self.balances[i] += self.balances[i-1] * self.interest_yield
             
     def annuitize(self,date):
-        self.payments = self.balances[self.time_ls.index(date)] * self.payout_rate
+        self.annuitized = True
+        self.annuitized_payments = self.balances[self.time_ls.index(date)] * self.payout_rate
     
-    def income(self,time):
-        pass
+    def take_payment(self,date):
+        if self.annuitized:
+            return self.annuitized_payments
+        else:
+            payment =  self.balances[self.time_ls.index(date)] * self.payout_rate
+            self.transactions[self.time_ls.index(date)] -= payment
+            self._update_balances()
+            return payment
     
     
 if __name__ == '__main__':
-    annuity = Annuity(interest_rate_qt=1.001,payout_rate_qt=0.01,time_ls=[2022,2022.25,2022.5,2022.75,2023,2023.25,2023.5,2023.75])
+    annuity = Annuity(interest_yield_qt=1.001,payout_rate_qt=0.01,time_ls=[2022,2022.25,2022.5,2022.75,2023,2023.25,2023.5,2023.75])
     annuity.contribute(100,2022)
     annuity.contribute(100,2022.5)
-    annuity.annuitize(2023.5)
+    #annuity.annuitize(2023.5)
+    print(annuity.take_payment(2023.75))
     debug=True
