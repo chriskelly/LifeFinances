62e5f6377d4c4c57b214d8efdb1c7b8c0e26f697
kellychris_work <christopherkelly92@gmail.com>
2022-09-03 18:33:35 -0700

    improved genetic.py comments

--- genetic.py
+++ genetic.py
@@ -7,9 +7,9 @@ import data.constants as const
 import numpy as np # used in eval() of parameter ranges
 import scipy.stats as ss
 
-DEBUG_LVL = 1
-RESET_SUCCESS = False
-SUCCESS_THRESH = 0.5
+DEBUG_LVL = 1 # Lvl 0 shows only local and final max param sets
+RESET_SUCCESS = False # Set to true to reset all the counts in param_success.json
+SUCCESS_THRESH = 0.5 # Initial threshold for random mutations to beat before switching to step mutations
 OFFSPRING_QTY = 5
 TARGET_SUCCESS_RATE = 0.95
 MAX_MONTE_RUNS = 5000
@@ -18,35 +18,35 @@ RNG = np.random.default_rng()
 
 class Algorithm:
     def __init__(self):
-        self.prev_used_params = []
+        self.prev_used_params = [] # used to track and prevent reusing the same param sets during step mutation
         simulator.DEBUG_LVL = 0
         with open(const.PARAMS_SUCCESS_LOC) as json_file:
             self.param_cnt = json.load(json_file)
         if RESET_SUCCESS: self.param_cnt = {}
     
-    # ---------------------- Initialization ---------------------- #
     def main(self, next_loop=(False,[])):
+    # ---------------------- First parameter set ---------------------- #
         self.model = Model()
         mutable_params = self.model.filter_params(include=True, attr='range')
         success_rate, parent_is_best_qty = 0.0 , 0
         if next_loop[0]: # check to see if this is the first loop or if the previous one was successful and we're auto-advancing
             full_params = next_loop[1]
             parent_mute_params = mutable_params
-        else:
+        else: # if not, keep random mutating till we hit SUCCESS_THRESH
             full_params = copy.deepcopy(self.model.params) # make a copy rather than point to the same dict # https://stackoverflow.com/a/22341377/13627745
             while success_rate <  SUCCESS_THRESH:
                 success_rate, parent_mute_params = self._make_child(full_params,mutable_params,mutate='random')
                 if DEBUG_LVL>=1: print(f"Success Rate: {success_rate*100:.2f}%")
-        # Plot first parameters
         self._update_param_count(mutable_params,first_time=True)
+    # ---------------------- Improvement loop ---------------------- #
         while True: # while success_rate <  TARGET_SUCCESS_RATE      if you every want to stop the auto-advance
             # Make children
             children = []
             for _ in range(OFFSPRING_QTY):
                 children.append(self._make_child(full_params,parent_mute_params,mutate='step',max_step=max(1,parent_is_best_qty)))
-            # Breed from best child (or parent if all children worse)
+            # Find best child (or use parent if all children worse)
             children.sort(key=lambda u: u[0], reverse=True) # Needed to avoid it sorting by the params if success rates are equal
-            # Compare parent to best child
+            # ------ Children not improving ------ #
             if success_rate >= children[0][0]: # Parent better than child
                 parent_is_best_qty += 1
                 if DEBUG_LVL>=1: print(f"No better children {parent_is_best_qty}/10")
@@ -57,12 +57,13 @@ class Algorithm:
                     success_rate = 0.0
                     while success_rate <  SUCCESS_THRESH:
                         success_rate, parent_mute_params = self._make_child(full_params,mutable_params,mutate='random')
+            # ------ Child is better ------ #
             else: # If child better than parent, update success rate and params
                 parent_is_best_qty = 0
                 success_rate, parent_mute_params = children[0] 
                 self._update_param_count(parent_mute_params)
                 if DEBUG_LVL>=1: print(f"Success Rate: {success_rate*100:.2f}%")
-            # If child meets target, test the results to the max before ending routine
+            # ------ Child beats target, proceed to test child ------ #
             if success_rate >= TARGET_SUCCESS_RATE: 
                 current_monte_carlo_runs = simulator.MONTE_CARLO_RUNS # save previous value
                 simulator.MONTE_CARLO_RUNS = MAX_MONTE_RUNS
@@ -71,7 +72,7 @@ class Algorithm:
                 if success_rate < TARGET_SUCCESS_RATE: 
                     if DEBUG_LVL>=1:
                         print(f"Couldn't stand the pressure...{success_rate*100:.2f}%")
-                else:
+                else: # Print results, overwrite params, start again with more ambitious FI target date
                     param_vals = {key:obj["val"] for (key,obj) in parent_mute_params.items()}
                     print(f"Final max: {success_rate*100:.2f}%\n {param_vals}")
                     full_params.update(parent_mute_params)
@@ -85,12 +86,15 @@ class Algorithm:
     
     # ---------------------- Mutation ---------------------- #
     def _random_mutate(self,mutable_params):
+        """Return mutable params with shuffled values"""
         new_dict = copy.deepcopy(mutable_params) 
         for param,obj in new_dict.items():
             new_dict[param]['val'] = str(random.choice(eval(obj["range"])))
         return new_dict
     
     def _step_mutate(self,mutable_params,max_step=1):
+        """Return mutable params with values shifted in a normal distribution around 
+        provided mutable_param values with a max deviation of max_step"""
         new_dict = copy.deepcopy(mutable_params)
         for param,obj in new_dict.items():
             ls = list(eval(obj["range"]))
@@ -123,6 +127,9 @@ class Algorithm:
         return np.random.choice(x, p = prob)
     
     def _update_param_count(self,mutable_params:dict,first_time=False):
+        """Edit the param_success.json file to add another tally for each of the 
+        successful mutable_param values. If first time and RESET_SUCCESS, 
+        overwrite previous file and set count to 0"""
         if RESET_SUCCESS and first_time:
             for param,obj in mutable_params.items():
                 self.param_cnt[param] = {}
