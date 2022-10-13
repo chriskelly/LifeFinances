9ddf19f50dbfef24146eb2e18908437429992f42
kellychris_work <christopherkelly92@gmail.com>
2022-10-13 16:53:13 -0700

    remove temp files from git

--- test1.py
+++
@@ -1,8 +0,0 @@
-import models.socialSecurity
-
-models.socialSecurity.test_unit()
-
-# import models.returnGenerator
-
-# stock_return_arr,bond_return_arr,re_return_arr,inflation_arr = models.returnGenerator.main(10,4,1)
-# debug =True
