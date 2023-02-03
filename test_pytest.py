9811748f0330e4d4e7d094844c9c8fc8cddd9d62
kellychris_work <christopherkelly92@gmail.com>
2023-02-02 22:41:29 -0800

    move test scripts

--- test_pytest.py
+++
@@ -1,8 +0,0 @@
-# python -m pytest test_pytest.py
-
-from models import model
-
-def test_param_loading():
-    # confirm that all parameter names in the details are also in the values dict and vice versa
-    vals,details = model.load_params()
-    assert not (details.keys() ^ vals.keys())
