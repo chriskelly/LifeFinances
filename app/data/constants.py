c0276dc3b46403cb6c49796c33a9708c41963ff3
Chris Kelly <christopherkelly92@gmail.com>
2025-12-03 14:35:05 -0500

    chore: add recent years' maximum earnings and indexes for Social Security, updated bend points, and included references for data sources.

--- app/data/constants.py
+++ app/data/constants.py
@@ -29,7 +29,7 @@ MONTHS_PER_INTERVAL = round(12 / INTERVALS_PER_YEAR)
 PENSION_INFLATION = 1.03
 """Expected increase yield to Social Security and Pension figures"""
 
-SS_MAX_EARNINGS = [
+SS_MAX_EARNINGS = [  # https://www.ssa.gov/benefits/retirement/planner/maxtax.html
     [2002, 84.900],
     [2003, 87.000],
     [2004, 87.900],
@@ -50,34 +50,40 @@ SS_MAX_EARNINGS = [
     [2019, 132.900],
     [2020, 137.700],
     [2021, 142.800],
+    [2022, 147.000],
+    [2023, 160.200],
+    [2024, 168.600],
+    [2025, 176.100],
 ]
 """List of historic data in format: [year,social security max earnings]"""
-SS_INDEXES = [
-    [2002, 1.7665978],
-    [2003, 1.7244432],
-    [2004, 1.6478390],
-    [2005, 1.5896724],
-    [2006, 1.5198170],
-    [2007, 1.4538392],
-    [2008, 1.4211470],
-    [2009, 1.4429071],
-    [2010, 1.4095913],
-    [2011, 1.3667660],
-    [2012, 1.3253803],
-    [2013, 1.3086540],
-    [2014, 1.2637941],
-    [2015, 1.2213044],
-    [2016, 1.2076578],
-    [2017, 1.1673463],
-    [2018, 1.1265158],
-    [2019, 1.0858240],
-    [2020, 1.0559868],
-    [2021, 1.0000000],
+SS_INDEXES = [  # https://www.ssa.gov/cgi-bin/awiFactors.cgi
+    [2003, 1.9557287],
+    [2004, 1.8688502],
+    [2005, 1.8028823],
+    [2006, 1.7236577],
+    [2007, 1.6488308],
+    [2008, 1.6117539],
+    [2009, 1.6364325],
+    [2010, 1.5986484],
+    [2011, 1.5500792],
+    [2012, 1.5031428],
+    [2013, 1.4841731],
+    [2014, 1.4332965],
+    [2015, 1.3851081],
+    [2016, 1.3696311],
+    [2017, 1.3239129],
+    [2018, 1.2776063],
+    [2019, 1.2314568],
+    [2020, 1.1976178],
+    [2021, 1.0998221],
+    [2022, 1.0443086],
+    [2023, 1.0000000],
+    [2024, 1.0000000],
 ]
 """List of historic data in format: [year,social security indicies]"""
-SS_BEND_POINTS = [1.024, 6.172]
+SS_BEND_POINTS = [1.286, 7.749]  # https://www.ssa.gov/oact/cola/bendpoints.html
 """Bend points in $1000s in format: [low bend point, high bend point]"""
-PIA_RATES = [0.9, 0.32, 0.15]
+PIA_RATES = [0.9, 0.32, 0.15]  # https://www.ssa.gov/oact/cola/piaformula.html
 """PIA rates in format: [rate below low bend point, rate between bend points,
 rate above high bend point]"""
 PIA_RATES_PENSION = [0.4, 0.32, 0.15]
