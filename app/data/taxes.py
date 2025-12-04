e653c6254b7ad6f7d780b8ca6047cba5d175da73
Chris Kelly <christopherkelly92@gmail.com>
2025-12-04 08:38:33 -0500

    chore: updated federal and california brackets. Moved third column generating code into taxes.ipynb

--- app/data/taxes.py
+++ app/data/taxes.py
@@ -4,72 +4,61 @@ This module contains the constants related to taxes across all supported states.
 
 """
 
-FED_STD_DEDUCTION = [12.950, 25.900]
-"""2022 federal standard deduction"""
+# Use `taxes.ipynb` to generate the third column of the bracket rates
+
+FED_STD_DEDUCTION = [15.000, 30.000]
+"""2025 federal standard deduction"""
 FED_BRACKET_RATES = [
     [
-        [0.1, 10.275, 0],
-        [0.12, 41.775, 1.027],
-        [0.22, 89.075, 4.807],
-        [0.24, 170.050, 15.213],
-        [0.32, 215.950, 34.647],
-        [0.35, 539.900, 49.335],
-        [0.37, float("inf"), 162.718],
+        [0.1, 11.925, 0],
+        [0.12, 48.475, 1.193],
+        [0.22, 103.350, 5.579],
+        [0.24, 197.300, 17.651],
+        [0.32, 250.525, 40.199],
+        [0.35, 626.350, 57.231],
+        [0.37, float("inf"), 188.77],
     ],
     [
-        [0.1, 20.500, 0],
-        [0.12, 83.550, 2.05],
-        [0.22, 178.150, 9.616],
-        [0.24, 340.100, 30.428],
-        [0.32, 431.900, 69.296],
-        [0.35, 647.850, 98.672],
-        [0.37, float("inf"), 174.254],
+        [0.1, 23.85, 0],
+        [0.12, 96.95, 2.385],
+        [0.22, 206.7, 11.157],
+        [0.24, 394.6, 35.302],
+        [0.32, 501.05, 80.398],
+        [0.35, 751.6, 114.462],
+        [0.37, float("inf"), 202.154],
     ],
 ]
 """2022 federal brackets for income tax in format
 [rate,highest dollar that rate applies to,sum of tax owed in previous brackets]"""
 
-# Code to calc third column of bracket rates
-# from data import taxes
-# brackets_set = taxes.STATE_BRACKET_RATES['New York']
-# rate_idx, cap_idx = 0, 1
-# for brackets in brackets_set:
-#     res = [0,round(brackets[0][rate_idx] * brackets[0][cap_idx], 3)] # first 2
-#     for i in range(1,len(brackets)-1):
-#         res.append(round(res[-1] + brackets[i][rate_idx]*(brackets[i][cap_idx]
-#                                                           -brackets[i-1][cap_idx]), 3))
-#     for i in range(len(res)-1):
-#         print(f'{[brackets[i][rate_idx], brackets[i][cap_idx], res[i]]},')
-#     print(f"[{brackets[-1][rate_idx]}, float('inf'), {res[-1]}]")
-
 STATE_STD_DEDUCTION = {
-    "California": [4.803, 9.606],  # 2022
+    "California": [5.540, 11.080],  # 2025
     "New York": [8.000, 16.050],  # 2022
 }
 """State standard deduction. state:[single, married]"""
 STATE_BRACKET_RATES = {
-    "California": [  # 2022
+    "California": [  # 2025
         [
-            [0.01, 9.325, 0],
-            [0.02, 22.107, 0.093],
-            [0.04, 34.892, 0.348],
-            [0.06, 48.435, 0.860],
-            [0.08, 61.214, 1.672],
-            [0.093, 312.686, 2.695],
-            [0.103, 375.221, 26.082],
-            [0.113, 625.369, 32.523],
-            [0.123, float("inf"), 60.789],
+            [0.01, 10.756, 0],
+            [0.02, 25.499, 0.108],
+            [0.04, 40.245, 0.403],
+            [0.06, 55.866, 0.993],
+            [0.08, 70.606, 1.93],
+            [0.093, 360.659, 3.109],
+            [0.103, 432.787, 30.084],
+            [0.113, 721.314, 37.513],
+            [0.123, float("inf"), 70.117],
         ],
         [
-            [0.01, 18.649, 0],
-            [0.02, 44.213, 0.186],
-            [0.04, 69.783, 0.698],
-            [0.06, 96.869, 1.720],
-            [0.08, 122.427, 3.346],
-            [0.093, 625.371, 5.390],
-            [0.103, 750.442, 52.164],
-            [0.113, 1250.738, 65.046],
-            [0.123, float("inf"), 121.580],
+            [0.01, 21.512, 0],
+            [0.02, 50.998, 0.215],
+            [0.04, 80.49, 0.805],
+            [0.06, 111.732, 1.985],
+            [0.08, 141.212, 3.86],
+            [0.093, 721.318, 6.218],
+            [0.103, 865.574, 60.168],
+            [0.113, 1442.628, 75.026],
+            [0.123, float("inf"), 140.233],
         ],
     ],
     "New York": [  # 2022
